96e3fd421b2f5cad955b96712a99b14382e0d9ed
russelltxs <79345359+russelltxs@users.noreply.github.com>
2023-04-10 18:25:55 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+const memcached = require('memcached');
+
+let memC = new memcached('localhost:11211');
+
 const app = express();
 
 app.get('/', (req,res) => {
   res.send('here we are');
 });
- 
+
+app.get('/memc/', (req, res) => {
+    res.send(   '<br>memc gameCode details for:<br>' +
+                '<br>get/GC_linked_users<br>' +
+                '<br>get/GC_game_status<br>' +
+                '<br>get/GC_user_results<br>' +
+                '<br>get/GC_user_ranking<br>' +
+                '<br>get/GC_teacher_details'
+    );
+});
+
  app.listen(5000, () => {
   console.log('App listening on port 5000!');
  });
