d8e59ad80cd179004470dbd2360fe701ad5fccec
russelltxs <79345359+russelltxs@users.noreply.github.com>
2023-04-10 19:29:16 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
-
 const memcached = require('memcached');
 
 let memC = new memcached('localhost:11211');
-
 const app = express();
 
 app.get('/', (req,res) => {
   res.send('here we are');
 });
 
+app.get('/memc/set/:key/:value', (req, res) => { // memC.set(key, value, 600);
+    memC.set(req.params.key, req.params.value, 600, (err) => { if (err) { res.send(err); }
+        res.send('The string key:value SET = '+req.params.key+':'+req.params.value );  
+    });
+});
+
+app.get('/memc/get/:key', (req, res) => { // memC.get(key, (err, data) => { do stuff with data
+    memC.get(req.params.key, (err, data) => { res.send( 'The retrieved GET value is: '+data ); }); 
+});
+
 app.get('/memc/', (req, res) => {
-    res.send(   '<br>memc gameCode details for:<br>' +
-                '<br>get/GC_linked_users<br>' +
-                '<br>get/GC_game_status<br>' +
-                '<br>get/GC_user_results<br>' +
-                '<br>get/GC_user_ranking<br>' +
-                '<br>get/GC_teacher_details'
+    res.send(   '<br>memc/set/:key/:value   (will set value to key for 600 sec)' +
+                '<br>memc/get/:key          (will retrieve vale for key * display)<br>'
     );
 });
 
